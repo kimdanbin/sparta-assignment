d23657151467032fba4987c679a9baaa466cdca7
kimdanbin <rlaeksqls@naver.com>
2024-11-29 10:44:47 +0900

    레벨 1-2: 고객과 통화 테이블 간 연관관계

--- src/main/java/com/sparta/currency_user/entity/User.java
+++ src/main/java/com/sparta/currency_user/entity/User.java
@@ -2,16 +2,28 @@ package com.sparta.currency_user.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
-public class User {
+@Table(name = "user")
+public class User extends Base{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Exchange> exchange;
 
     public User(String name, String email) {
         this.name = name;

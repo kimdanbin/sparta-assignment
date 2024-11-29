f5adb9d16ee625e490dc5866565bdbe018c9ee84
kimdanbin <rlaeksqls@naver.com>
2024-11-29 10:51:13 +0900

    레벨 3-1: 유효성 검사 추가

--- src/main/java/com/sparta/currency_user/controller/UserController.java
+++ src/main/java/com/sparta/currency_user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sparta.currency_user.controller;
 import com.sparta.currency_user.dto.UserRequestDto;
 import com.sparta.currency_user.dto.UserResponseDto;
 import com.sparta.currency_user.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,21 +17,26 @@ public class UserController {
 
     private final UserService userService;
 
+    // 전체 유저 조회
     @GetMapping
     public ResponseEntity<List<UserResponseDto>> findUsers() {
         return ResponseEntity.ok().body(userService.findAll());
     }
 
+    // 특정 유저 조회
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDto> findUser(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.findById(id));
     }
 
+    // 유저 생성
     @PostMapping
-    public ResponseEntity<UserResponseDto> createUser(@RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserRequestDto userRequestDto) {
         return ResponseEntity.ok().body(userService.save(userRequestDto));
     }
 
+    // 유저 삭제
+    // D: 고객이 삭제될 때 해당 고객이 수행한 모든 환전 요청도 삭제
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
         userService.deleteUserById(id);
--- src/main/java/com/sparta/currency_user/service/UserService.java
+++ src/main/java/com/sparta/currency_user/service/UserService.java
@@ -5,10 +5,13 @@ import com.sparta.currency_user.dto.UserResponseDto;
 import com.sparta.currency_user.entity.User;
 import com.sparta.currency_user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +32,12 @@ public class UserService {
 
     @Transactional
     public UserResponseDto save(UserRequestDto userRequestDto) {
+        // 이메일 형식 조건
+        String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        Pattern emailPattern = Pattern.compile(EMAIL_REGEX);
+        if (!emailPattern.matcher(userRequestDto.getEmail()).matches()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "이메일 형식으로 작성해주십시오");
+        }
         User savedUser = userRepository.save(userRequestDto.toEntity());
         return new UserResponseDto(savedUser);
     }
